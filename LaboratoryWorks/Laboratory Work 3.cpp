e28819ed5e42402eb6c598c9e92b001f964a2e02
Sergey Bogatov <moonygamesstudio@gmail.com>
2021-09-27 23:33:31 +0300

    Main version "Laboratory Work 3.cpp"

---
+++ LaboratoryWorks/Laboratory Work 3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    float a, b;
+    
+    cin >> a >> b;
+    
+    cout << "P = " << 2 * (a + b) << " S = " << a * b << endl;
+}
