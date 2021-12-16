5f15180c2d6487ecfa71db7d53ff6eb165b5f8e2
Sergey Bogatov <91119412+MoonyGames@users.noreply.github.com>
2021-12-16 14:10:18 +0300

    Added #2

---
+++ LaboratoryWorks/Лабораторная работа 9/2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std; //Подключили пространство имен std, чтобы не писать std каждый раз уточнение при использовании функции из библиотеки
+
+int main(int argc, const char * argv[]) {
+    int day, number;
+    string dayNames[7] = {
+        "понедельник",
+        "вторник",
+        "среда",
+        "четверг",
+        "пятница",
+        "суббота",
+        "воскресенье"
+    };
+    
+    cout << "Введите день года по счету от 1 до 365: ";
+    cin >> day;
+    
+    cout << "Это " << dayNames[day % 7 - 1];
+    
+    return 0;
+}
