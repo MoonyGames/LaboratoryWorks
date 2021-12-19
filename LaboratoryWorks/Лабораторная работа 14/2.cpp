f6feafc711e4e33202e73137e1eb732fe21cefa6
Sergey Bogatov <91119412+MoonyGames@users.noreply.github.com>
2021-12-19 16:59:50 +0300

    Added #2

--- LaboratoryWorks/Лабораторная работа 14/2.cpp
+++ LaboratoryWorks/Лабораторная работа 14/2.cpp
@@ -1,8 +1,19 @@
-//
-//  2.cpp
-//  LaboratoryWorks
-//
-//  Created by Сергей Богатов on 19.12.2021.
-//
+#include <iostream>
+#include <math.h>
 
-#include <stdio.h>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int a, b;
+    
+    cout << "Введите числа A и B через пробел: ";
+    cin >> a >> b;
+    
+    while (b < a){
+        a -= b;
+    }
+    
+    cout << "Незанятая часть: " << a;
+    
+    return 0;
+}
