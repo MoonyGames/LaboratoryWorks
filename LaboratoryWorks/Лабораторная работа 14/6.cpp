080c7c9c03ef64cefa58d0dc0b20d77a5f697317
Sergey Bogatov <91119412+MoonyGames@users.noreply.github.com>
2021-12-21 15:54:23 +0300

    Added #6

--- LaboratoryWorks/Лабораторная работа 14/6.cpp
+++ LaboratoryWorks/Лабораторная работа 14/6.cpp
@@ -2,9 +2,30 @@
 #include <math.h>
 
 using namespace std;
-
-int main(int argc, const char * argv[]) {
+ 
+int fib(int n){
+    int f = 0, f0 = 1, res = 1, i = 0;
     
+    while (i <= n){
+        res = f;
+        f = f0 + f;
+        f0 = res;
+        i++;
+        
+        if (n == f) return i;
+    }
+    
+    if (n == 0) return 0;
+    else return -1;
+}
+ 
+int main(int argc, const char * argv[]){
+    int n;
+    
+    cout << "Введите N: ";
+    cin >> n;
+    
+    cout << "Порядковый номер числа фибоначи N: " << fib(n) + 1;
     
     return 0;
 }
