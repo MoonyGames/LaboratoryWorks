f63d85877615ee89a5c2b1b82ba2fd6f95e6c5af
Sergey Bogatov <91119412+MoonyGames@users.noreply.github.com>
2021-12-21 16:28:38 +0300

    Added #2

--- LaboratoryWorks/Лабораторная работа 15/2.cpp
+++ LaboratoryWorks/Лабораторная работа 15/2.cpp
@@ -1,8 +1,20 @@
-//
-//  2.cpp
-//  LaboratoryWorks
-//
-//  Created by Сергей Богатов on 21.12.2021.
-//
+#include <iostream>
+#include <math.h>
 
-#include <stdio.h>
+using namespace std;
+
+int Sign(float x){
+    if (x < 0) return -1;
+    else if (x > 0) return 1;
+    else return 0;
+    
+    return 0;
+}
+
+int main(int argc, const char * argv[]) {
+    float a = 1.4, b = -2.5;
+    
+    cout << Sign(a) + Sign(b);
+    
+    return 0;
+}
