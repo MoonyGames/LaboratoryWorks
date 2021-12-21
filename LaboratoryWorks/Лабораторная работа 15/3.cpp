cc4934c08309e098dc0fe5f7b6c2e904f926f382
Sergey Bogatov <91119412+MoonyGames@users.noreply.github.com>
2021-12-21 17:02:50 +0300

    Added #3

--- LaboratoryWorks/Лабораторная работа 15/3.cpp
+++ LaboratoryWorks/Лабораторная работа 15/3.cpp
@@ -1,8 +1,18 @@
-//
-//  3.cpp
-//  LaboratoryWorks
-//
-//  Created by Сергей Богатов on 21.12.2021.
-//
+#include <iostream>
+#include <math.h>
 
-#include <stdio.h>
+using namespace std;
+
+float RingS(float R1, float R2){
+    return R1 > R2 ? M_PI * (R1 * R1 - R2 * R2) : M_PI * (R2 * R2 - R1 * R1);
+}
+
+int main(int argc, const char * argv[]) {
+    float S1 = RingS(6, 7);
+    float S2 = RingS(7, 8);
+    float S3 = RingS(9, 10);
+    
+    cout << S1 << endl << S2 << endl << S3;
+    
+    return 0;
+}
