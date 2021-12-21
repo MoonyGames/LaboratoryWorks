e15ccb984de4209bec895dd299ebec13f669d534
Sergey Bogatov <91119412+MoonyGames@users.noreply.github.com>
2021-12-21 15:37:22 +0300

    Added #5

--- LaboratoryWorks/Лабораторная работа 14/5.cpp
+++ LaboratoryWorks/Лабораторная работа 14/5.cpp
@@ -1,8 +1,20 @@
-//
-//  5.cpp
-//  LaboratoryWorks
-//
-//  Created by Сергей Богатов on 19.12.2021.
-//
+#include <iostream>
+#include <math.h>
 
-#include <stdio.h>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int s = 1000, p, i = 0;
+    
+    cout << "Введите процентную ставку на вклад: ";
+    cin >> p;
+    
+    while (s < 1100){
+        s += s * (p / 100);
+        i++;
+    }
+    
+    cout << "Итоговая сумма: " << s << ". Эта сумма накопится за " << i << " месяцев";
+    
+    return 0;
+}
