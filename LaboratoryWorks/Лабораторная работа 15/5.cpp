52d48f3000684f9b7acb15667353311ec046283e
Sergey Bogatov <91119412+MoonyGames@users.noreply.github.com>
2021-12-22 01:07:28 +0300

    Added #5

--- LaboratoryWorks/Лабораторная работа 15/5.cpp
+++ LaboratoryWorks/Лабораторная работа 15/5.cpp
@@ -1,8 +1,32 @@
-//
-//  5.cpp
-//  LaboratoryWorks
-//
-//  Created by Сергей Богатов on 21.12.2021.
-//
+#include <iostream>
+#include <math.h>
 
-#include <stdio.h>
+using namespace std;
+
+float Fact2(int n){
+    float sum = 1;
+    
+    if (n % 2 == 0){
+        for (int i = 2; i <= n; i += 2){
+            sum *= i;
+        }
+    }
+    else{
+        for (int i = 1; i <= n; i += 2){
+            sum *= i;
+        }
+    }
+    
+    return sum;
+}
+
+int main(int argc, const char * argv[]) {
+    int n;
+    
+    cout << "Введите число N: ";
+    cin >> n;
+    
+    cout << Fact2(n);
+    
+    return 0;
+}
