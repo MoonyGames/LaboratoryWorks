6988bf31b145eecbe4b86b15e46e93b5d1b363a9
Sergey Bogatov <91119412+MoonyGames@users.noreply.github.com>
2021-12-24 20:12:14 +0300

    Added #2

---
+++ LaboratoryWorks/Лабораторная работа 17/2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int n, count = 0;
+    
+    cout << "Введите размер массива: ";
+    cin >> n;
+    
+    int a[n];
+    
+    for (int i = 0; i < n; i++){
+        cout << "Введите " << i << " элемент массива: " << endl;
+        cin >> a[i];
+    }
+    
+    for (int i = 1; i < n - 1; i++){ if(a[i + 1] - a[i] == a[i] - a[i - 1]) count++;
+    
+    if (count + 2 == n) cout << "Разность прогрессии: " << a[1] - a[0];
+    else cout << "0";
+    
+    return 0;
+}
