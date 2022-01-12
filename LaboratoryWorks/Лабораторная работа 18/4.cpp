dd54418d4998669fdab8d18e9a1efab89011d127
Sergey Bogatov <91119412+MoonyGames@users.noreply.github.com>
2022-01-12 13:00:00 +0300

    Added #4

---
+++ LaboratoryWorks/Лабораторная работа 18/4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int n, min, max = 0, minIndex, maxIndex;
+    
+    cout << "Введите размер массива: ";
+    cin >> n;
+    
+    int a[n];
+    
+    for (int i = 0; i < n; i++){
+        cout << "Введите " << i << " элемент массива A: " << endl;
+        cin >> a[i];
+    }
+    
+    for (int i = 0; i < n; ++i){
+        if (a[i] > max){
+            max = a[i];
+            maxIndex = i;
+        }
+    }
+    
+    min = max;
+    
+    for (int i = 0; i < n; ++i){
+        if (a[i] < min){
+            min = a[i];
+            minIndex = i;
+        }
+    }
+    
+    if (minIndex < maxIndex) for (int i = minIndex + 1; i < maxIndex; i++) a[i] = 0;
+    else for (int i = maxIndex + 1; i < minIndex; i++) a[i] = 0;
+    
+    for(int i = 0; i < n; i++) cout << i << " элемент массива = " << a[i] << endl;
+}
